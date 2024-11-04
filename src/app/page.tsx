3d95f85f6deb09df306c20c48b2aa407ff4e4555
Lars Götte <larsgoette@icloud.com>
2024-11-04 15:31:40 +0100

    added main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { SchemaOrg } from "@/components/features/SchemaOrg";
 import { BreadcrumbList, WithContext } from "schema-dts";
-import { BASE_URL } from "@/app/config";
+import { APP_DESCRIPTION, APP_NAME, APP_TITLE, BASE_URL } from "@/app/config";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: `${APP_TITLE} | ${APP_NAME}`,
+  },
+  description: APP_DESCRIPTION,
+};
 
 const breadcrumbListSchema: WithContext<BreadcrumbList> = {
   "@context": "https://schema.org",
@@ -19,7 +27,14 @@ export default function Home() {
   return (
     <>
       <main>
-        <h1 className="font-variation-bold text-base">Hello World!</h1>
+        <section className="min-h-dvh py-20">
+          <hgroup className="flex flex-col gap-2">
+            <h1 className="text-5xl font-bold">{APP_TITLE}</h1>
+            <p className="font text-5xl font-bold text-neutral-400">
+              {APP_DESCRIPTION}
+            </p>
+          </hgroup>
+        </section>
       </main>
       <SchemaOrg schema={breadcrumbListSchema} />
     </>
