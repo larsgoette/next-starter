4c82331a005f7a2dc85693d8f0f69569471c0425
Lars Götte <larsgoette@icloud.com>
2024-11-05 11:43:31 +0100

    minor design changes

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="my-4 flex gap-4">
-      <span className="text-sm font-semibold">{APP_NAME}</span>
+      <Link href="/" className="text-sm font-semibold">
+        {APP_NAME}
+      </Link>
       <nav className="text-sm">
         <ul className="flex gap-4">
           <li>
             <Link
               href="/"
-              className="text-neutral-500 transition-colors duration-300 ease-in-out hover:text-neutral-950">
+              className="text-zinc-500 transition-colors duration-300 ease-in-out hover:text-zinc-950">
               Home
             </Link>
           </li>
