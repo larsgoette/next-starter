44d35c751d3407895f8e694de25a8202da975601
Lars Götte <larsgoette@icloud.com>
2024-12-05 12:14:25 +0100

    updated to tailwind v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-    "@csstools/postcss-oklab-function": { preserve: true },
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
 };
 
